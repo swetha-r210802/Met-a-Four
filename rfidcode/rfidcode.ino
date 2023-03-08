1c28e11ce8aff1fee2551bb34ccd91351cf3fab4
swetha-r210802 <83578769+swetha-r210802@users.noreply.github.com>
2023-03-08 15:26:02 +0530

    Add files via upload

---
+++ rfidcode/rfidcode.ino
@@ -0,0 +1,21 @@
+int count = 0;
+char card_no[12];
+void setup()
+{
+   Serial.begin(9600);
+}
+
+void loop()
+{
+   if(Serial.available())
+   {
+      count = 0;
+      while(Serial.available() && count < 12)
+      {
+         card_no[count] = Serial.read();
+         count++;
+         delay(5);
+      }
+      Serial.print(card_no);
+   }
+}
